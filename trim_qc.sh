d0da02f7d5983cbac22f3dea632859a395aad96d
KingSte3l <93422186+KingSte3l@users.noreply.github.com>
2024-01-13 16:59:43 +0530

    /trim_galore

--- trim_qc.sh
+++ trim_qc.sh
@@ -5,23 +5,16 @@
 # Set the output directory for the trimmed and quality-controlled files
 output_dir="./output/"
 
-# Create the output directory if it doesn't exist
-mkdir -p "$trim_output_dir"
 
 # Loop through the FASTQ files in the input directory
-for fastq_file in "$input_dir"*.fastq.gz; do
-    # Check if the file exists
-    if [ -e "$fastq_file" ]; then
-        # Extract the file name without extension (e.g., sample)
-        base_name="$(basename "$fastq_file" .fastq.gz)"
+for forward_file in "$input_dir"*_1.fastq.gz; do
+      # Extract the file name without extension (e.g., sample)
+      base_name="$(basename "$forward_file" _1.fastq.gz)"
+      reverse_file=${base_name}_2.fastq.gz
 
-        # Define the output file name for trimmed FASTQ
-        trimmed_fastq="$output_dir${base_name}_trimmed.fastq.gz"
+      # Run TrimGalore! with desired options (e.g., specifying quality scores, adapters, etc.)
+      trim_galore --paired "$forward_file" "$reverse_file" --quality 20  --output_dir "$output_dir" 
 
-        # Run TrimGalore! with desired options (e.g., specifying quality scores, adapters, etc.)
-        trim_galore --quality 20 --length 50 --fastqc --gzip --output_dir "$output_dir" "$fastq_file"
-
-    fi
 done
 
 echo "Trimming and quality control completed."
